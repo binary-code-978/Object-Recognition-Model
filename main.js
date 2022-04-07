f3ee49ffdc848df738cc7ee5794f6d77cfab15cb
binary-code-978 <93479009+binary-code-978@users.noreply.github.com>
2022-04-07 15:52:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ camera = document.getElementById("camera");
 Webcam.attach('#camera');
 
 function take_snapshot() {
-    webcam.snap(function (data_uri) {
+   Webcam.snap(function (data_uri) {
         document.getElementById("result").innerHTML = '<img id="captured_image"src=' + data_uri + '"/>';
     });
 }
